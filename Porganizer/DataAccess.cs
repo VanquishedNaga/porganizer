58a7556007426966d9bda60960c0bc3ab6b338da
Alex P <yinjiat.phoon@gmail.com>
2020-06-29 21:48:52 +0800

    Move VideoFile class to DataAccessLibrary

--- Porganizer/DataAccess.cs
+++ Porganizer/DataAccess.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
+using System.IO;
 using System.Runtime.CompilerServices;
 using Microsoft.Data.Sqlite;
 using Windows.Storage;
 using Windows.Storage.AccessCache;
 using Windows.Storage.FileProperties;
 using Windows.Storage.Search;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace Porganizer
@@ -298,6 +300,145 @@ namespace Porganizer
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class VideoFile : BindableBase
+    {
+        private static List<string> supportedVideoTypes = new List<string>
+        {
+            ".avi",
+            ".mkv",
+            ".mov",
+            ".mp4",
+            ".wmv",
+        };
+
+        public static bool IsSupportedVideoType(StorageFile file)
+        {
+            bool isVideo = false;
+
+            if (supportedVideoTypes.Contains(file.FileType))
+            {
+                isVideo = true;
+            }
+
+            return isVideo;
+        }
+
+        public VideoFile()
+        {
+        }
+
+        public VideoFile(StorageFile inputFile)
+        {
+            file = inputFile;
+
+            GetThumbnail();
+            FindScreens();
+            FindGif();
+        }
+
+        private async void GetThumbnail()
+        {
+            BitmapImage image = new BitmapImage();
+
+            var temp = await file.GetThumbnailAsync(ThumbnailMode.VideosView);
+            if (temp != null)
+            {
+                await image.SetSourceAsync(temp);
+                Thumbnail = image;
+                DisplayedImage = image;
+            }
+        }
+
+        private async void FindScreens()
+        {
+            List<string> fileTypeFilter = new List<string>
+            {
+                ".jpg"
+            };
+
+            var queryOptions = new QueryOptions(CommonFileQuery.DefaultQuery, fileTypeFilter)
+            {
+                ApplicationSearchFilter = "System.FileName:*\"" + Path.GetFileNameWithoutExtension(File.Name) + "\"*"
+            };
+
+            StorageFolder folder = await File.GetParentAsync();
+            StorageFileQueryResult queryResult = folder.CreateFileQueryWithOptions(queryOptions);
+
+            var files = await queryResult.GetFilesAsync();
+            if (files.Count > 0)
+            {
+                Screen = files[0];
+            }
+        }
+
+        private async void FindGif()
+        {
+            List<string> fileTypeFilter = new List<string>
+            {
+                ".gif"
+            };
+
+            var queryOptions = new QueryOptions(CommonFileQuery.DefaultQuery, fileTypeFilter)
+            {
+                ApplicationSearchFilter = "System.FileName:*\"" + Path.GetFileNameWithoutExtension(File.Name) + "\"*"
+            };
+
+            StorageFolder folder = await File.GetParentAsync();
+            StorageFileQueryResult queryResult = folder.CreateFileQueryWithOptions(queryOptions);
+
+            var files = await queryResult.GetFilesAsync();
+            if (files.Count > 0)
+            {
+                IRandomAccessStream fileStream = await files[0].OpenAsync(FileAccessMode.Read);
+                BitmapImage image = new BitmapImage();
+                image.SetSource(fileStream);
+                Gif = image;
+            }
+        }
+
+        private StorageFile file;
+        public StorageFile File
+        {
+            get { return this.file; }
+            set { this.SetProperty(ref this.file, value); }
+        }
+
+        private StorageFile screen;
+        public StorageFile Screen
+        {
+            get { return this.screen; }
+            set { this.SetProperty(ref this.screen, value); }
+        }
+
+        private BitmapImage thumbnail;
+        public BitmapImage Thumbnail
+        {
+            get { return this.thumbnail; }
+            set { this.SetProperty(ref this.thumbnail, value); }
+        }
+
+        private BitmapImage gif;
+        public BitmapImage Gif
+        {
+            get { return this.gif; }
+            set { this.SetProperty(ref this.gif, value); }
+        }
+
+        private BitmapImage displayedImage;
+        public BitmapImage DisplayedImage
+        {
+            get { return this.displayedImage; }
+            set { this.SetProperty(ref this.displayedImage, value); }
+        }
+
+        private BitmapImage screenImage;
+        public BitmapImage ScreenImage
+        {
+            get { return this.screenImage; }
+            set { this.SetProperty(ref this.screenImage, value); }
         }
     }
 
